6b895c8b61cb34ed3a46130df9959dbfe1dd6620
Nahuel Nina <cristian.nina@uab.edu.bo>
2025-04-14 03:01:40 -0400

    feat:ad base entity for role and userRole

--- Chaski.Domain/Entities/Users/UserRole.cs
+++ Chaski.Domain/Entities/Users/UserRole.cs
@@ -1,7 +1,16 @@
+using Chaski.Domain.Common;
+
 namespace Chaski.Domain.Entities.Users;
 
-public class UserRole
+public class UserRole:BaseEntity
 {
     public int UserId { get; private set; }
     public int RoleId { get; private set; }
+    
+    public UserRole(int id, int userId, int roleId)
+        : base(id)
+    {
+        UserId = userId;
+        RoleId = roleId;
+    }
 }
